c4bc1d18a981850b10aeae39941647d09f673904
fhindo <mjhindo@gmail.com>
2022-08-26 23:22:02 -0700

    update

--- ProjectGame/Data/Checker.cs
+++ ProjectGame/Data/Checker.cs
@@ -1,4 +1,5 @@
 namespace ProjectGame.Pages;
+using ProjectGame.Data;
 public class CheckerClass
 {
     public int Row { get; set; }
@@ -7,28 +8,50 @@ public class CheckerClass
     public CheckerDirection Direction { get; set; }
 
 
+    // these lists are to store the value of the cell that the checker can move to
     public List<int> rowsOptions = new List<int>();
     public List<int> coulmnsOptions = new List<int>();
-    public void CheckCheckerPosition()
-    {
-        rowsOptions.Clear();
-        coulmnsOptions.Clear();
-        if (Row != null && Column != null)
-        {
-            rowsOptions.Add(Row + (1 * (Direction == CheckerDirection.Down ? 1 : -1)));
-            coulmnsOptions.Add(Column - 1);
-            coulmnsOptions.Add(Column + 1);
-        }
-    }
+    
+    // public CheckerClass? activeChecker = null;
 
-    public void MoveChecker(int row, int column)
-    {
-        bool canMoveTo = rowsOptions.Contains(row) && coulmnsOptions.Contains(column);
-        if (!canMoveTo)
-            return;
-        Column = column;
-        Row = row;
-    }
+    
+
+    // public void CheckCheckerPosition()
+    // {
+    //     rowsOptions.Clear();
+    //     coulmnsOptions.Clear();
+    //     if (activeChecker != null)
+    //             {
+    //             Console.WriteLine(Row);
+    //             Console.WriteLine(Column);
+    //             Console.WriteLine(Color);
+    //             Console.WriteLine(Direction);
+    //             rowsOptions.Add(Row + (1* (activeChecker.Direction == CheckerDirection.Down ? 1 : -1)));
+    //             coulmnsOptions.Add(Column -1);
+    //             coulmnsOptions.Add(Column +1);
+    //             }
+        
+    // }
+
+    //  void MoveChecker( int row, int column )
+    // {
+    //     Console.WriteLine( "MoveChecker" );
+    //     bool canMoveHere = rowsPossible.Contains( row ) && columnsPossible.Contains( column );
+    //     if( !canMoveHere )
+    //     {
+    //         return;
+    //     }
+    //     if( activeChecker != null )
+    //     {
+    //         activeChecker.Column = column;
+    //         activeChecker.Row = row;
+    //         activeChecker = null;
+    //         EvaluateCheckerSpots();
+    //     }
+    
+    // }
+
+    
 
 }
 
@@ -36,3 +59,40 @@ public enum CheckerDirection
 {
     Down, Up, Both
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public void CheckCheckerPosition()
+    // {
+    //     rowsOptions.Clear();
+    //     coulmnsOptions.Clear();
+    //     if (Row != null && Column != null)
+    //     {
+    //         rowsOptions.Add(Row + (1 * (Direction == CheckerDirection.Down ? 1 : -1)));
+    //         coulmnsOptions.Add(Column - 1);
+    //         coulmnsOptions.Add(Column + 1);
+    //     }
+    // }
+
+    // public void MoveChecker(int row, int column)
+    // {
+    //     bool canMoveTo = rowsOptions.Contains(row) && coulmnsOptions.Contains(column);
+    //     if (!canMoveTo)
+    //         return;
+    //     Column = column;
+    //     Row = row;
+    // }
